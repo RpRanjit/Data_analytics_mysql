4067e567a413bbe35246ad6a145049d9abca3b11
Ranjit Poudel <ranjitsparrow2@gmail.com>
2026-06-11 16:10:30 +0545

    UNIONS and its examples

---
+++ UNIONS.sql
@@ -0,0 +1,40 @@
+-- UNION
+-- JOIn is use to join the columns of sam or multiple tabales
+-- But UNION is use to join rows from sam or multiple columns
+
+SELECT age, gender
+FROM parks_and_recreation.employee_demographics
+UNION
+SELECT first_name, last_name
+FROM parks_and_recreation.employee_salary;
+
+-- here  by default the union is distinct and we get unique character present in two tables
+SELECT first_name, last_name
+FROM parks_and_recreation.employee_demographics
+UNION 
+SELECT first_name, last_name
+FROM parks_and_recreation.employee_salary;
+
+-- here UNION ALL shows each and every character present in two tables even though they have duplicates
+SELECT first_name, last_name
+FROM parks_and_recreation.employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM parks_and_recreation.employee_salary;
+
+
+
+-- here  by default the union is distinct and we get unique character present in two tables
+SELECT first_name, last_name,  'Old man' AS Label 
+FROM parks_and_recreation.employee_demographics
+WHERE age > 40 AND gender = 'male'
+UNION
+SELECT first_name, last_name,  'Old lady' AS Label 
+FROM parks_and_recreation.employee_demographics
+WHERE age > 40 AND gender = 'female'
+UNION 
+SELECT first_name, last_name, 'Highly Paid Employee' AS Label
+FROM parks_and_recreation.employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name
+;
