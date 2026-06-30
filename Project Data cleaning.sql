c3f74a428d60d3d7cc2695913206f0d1a41f74bd
Ranjit Poudel <ranjitsparrow2@gmail.com>
2026-06-30 16:58:27 +0545

    UPDATE THE NULL AND BLANK IN industry column

--- Project Data cleaning.sql
+++ Project Data cleaning.sql
@@ -223,15 +223,15 @@ WHERE (t1.industry IS NULL OR t1.industry = '')
 AND t2.industry IS NOT NULL; 
 
 -- Now update it
+UPDATE layoffs_sample2
+SET industry = NULL
+WHERE industry = '';
 
 UPDATE layoffs_sample2 AS t1
 JOIN layoffs_sample2 AS t2
 	ON t1.company = t2.company
 SET t1.industry = t2.industry
-WHERE ( t1.industry IS NULL OR t1.industry = '')
+WHERE ( t1.industry IS NULL )
 AND t2.industry IS NOT NULL;
 
-SELECT *
-FROM layoffs_sample2
-WHERE industry IS NULL
-OR industry = '';
+
